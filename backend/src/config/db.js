dcb6f0a09cb52c5f0ca2096446c277d64e076ee0
RAKESH-TAMBOLI <rakeshtamboli006688@gmail.com>
2024-03-30 02:19:30 +0530

    testing 7

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,6 +8,7 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 // const MONGO_URL = "mongodb://127.0.0.1:27017/interprint"
 
 
+
 const MONGO_URL = "mongodb://rakeshtamboli006688:Mh6EBlSADVfEFnad@ac-qm5lva9-shard-00-00.q8bdnl4.mongodb.net:27017,ac-qm5lva9-shard-00-01.q8bdnl4.mongodb.net:27017,ac-qm5lva9-shard-00-02.q8bdnl4.mongodb.net:27017/?ssl=true&replicaSet=atlas-52qw5b-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 
 
